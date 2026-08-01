8df021f6e1b9c5274b66c2d81186dbb037ae021c
D4nD4n01 <danytazos@gmail.com>
2026-08-01 15:06:17 -0600

    20260108 1506

--- ligaproweb/src/components/jugador/partidos/PartidosPendientes.jsx
+++ ligaproweb/src/components/jugador/partidos/PartidosPendientes.jsx
@@ -1,22 +1,13 @@
 import { useEffect } from "react"
 
-function PartidosPendientes() {
-    useEffect = () => {
-        saludar()
-    }
-
-    function saludar() {
-        console.log("hola")
-    }
+export function PartidosPendientes() {
 
     return (
         <div>
             Hola, esto es partidos pendientes
-            <button onClick={() => saludar()}> boton</button>
-
         </div>
 
     )
 }
 
-export { PartidosPendientes }
+//export { PartidosPendientes }
